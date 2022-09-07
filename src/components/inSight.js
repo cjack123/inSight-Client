c7c9d428f76e91e65b31248c432d923a5f042972
Chaneice Jackson <c.jackson0126@gmail.com>
2022-09-06 19:23:33 -0500

    inSight client side

---
+++ src/components/inSight.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./inSight.css"
+
+export const Insight = () => {
+    
+    return (
+        <>
+            <h2> Welcome to inSight!</h2>
+            <p> Enter your gift cards and transactions</p>
+            <p> Then, we'll keep log of your transactions and calculate the new balance.</p>
+        </>
+    )
+} 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Insight } from './components/inSight'; 
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+    <Insight />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
