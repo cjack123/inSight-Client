09e35c2344fe9404c283ff04e264ef2444767d54
Chaneice Jackson <c.jackson0126@gmail.com>
2022-09-09 21:05:14 -0500

    application views work

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,19 +1,18 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { Home } from "./Home"
 import { GCard } from "./components/GiftCard/GCard"
 
 
 export const ApplicationViews = () => {
-    return (
-        <>
-            <Switch>
-                {/* Render the location list when http://localhost:3000/ */}
-                <Route exact path="/" element={<Home />} />
-
-                {/* Render the animal list when http://localhost:3000/giftcards */}
-                <Route path="/giftcards" element={<GCard />} />
-            </Switch>
+    return <>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/giftcards">
+                <GCard />
+            </Route>
+               
         </>
-    )
+    
 }
--- src/components/inSight.js
+++ src/components/inSight.js
@@ -1,16 +1,25 @@
 import React from "react"
-import { NavBar } from "./nav/NavBar"
+import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "../ApplicationViews"
+import { NavBar } from "./nav/NavBar"
 import "./inSight.css"
 
 export const Insight = () => {
     
     return (
         <>
-            <nav>
-                <NavBar />
-                <ApplicationViews />
-            </nav>
+        <Route render={() => {
+            if (localStorage.getItem("lu_token")) {
+                return <>
+                    <Route>
+                        <NavBar />
+                        <ApplicationViews />
+                    </Route>
+                </>
+            } else {
+                return <Redirect to="/login" />
+            }
+            }} />
         </>
     )
 } 
