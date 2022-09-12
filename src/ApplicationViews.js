d45c72c3ae2d62aba0a4a9700681220cc125af91
Chaneice Jackson <c.jackson0126@gmail.com>
2022-09-11 19:04:56 -0500

    Edit form complete

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom"
 import { Home } from "./Home"
 // import { GCard } from "./components/GiftCard/GCard"
 import { GCardList } from "./components/GiftCard/GCardList"
+import { GCardDetail } from "./components/GiftCard/GCardDetails"
 import { GCardForm } from "./components/GiftCard/GCardForm"
 import { UpdateCardForm } from "./components/GiftCard/GCardEditForm"
 
@@ -18,6 +19,9 @@ export const ApplicationViews = () => {
                 </Route> */}
             <Route exact path="/cards">
                 <GCardList />
+                </Route>
+            <Route exact path="/cards/:animalId">
+                <GCardDetail />
                 </Route>
             <Route exact path="/cards/new">
                 <GCardForm />
--- src/components/GiftCard/GCard.js
+++ src/components/GiftCard/GCard.js
@@ -9,19 +9,23 @@ export const GCard = ({ card, removeCard }) => {
         <>
 
         <article className="cards">
+
                     <section key={`card--${card.id}`} className="card">
+                    <br></br>
+                    <br></br>
                         <div className="card__number">Card Number: {card.card_number}</div>
-                        <div className="card__type">Card TYpe: {card.card_type}</div>
-                        <div className="card__expType">Expiration Date: {card.expiration_date}</div>
+                        <div className="card__type">Card Type: {card.card_type}</div>
+                        {/* <div className="card__expType">Expiration Date: {card.expiration_date}</div>
                         <div className="card__sCode">Security Code: {card.security_code}</div>
-                        <div className="card__secBalance">Initial Balance: {card.start_balance}</div>
+                        <div className="card__secBalance">Initial Balance: {card.start_balance}</div> */}
                         <div className="card__currBalance">Current Balance: {card.current_balance}</div>
                         <div className="buttons">
+                        <Link to={`/cards/${card.id}`}>
+                            <button>Card Details</button>
+                            </Link>
                         <button type="button" onClick={() => removeCard(card.id)}>Delete Card</button>
                         </div>
-                        <br></br>
-                        <br></br>
-                        <br></br>
+                    <br></br>
                     </section>
         </article>
         </>
